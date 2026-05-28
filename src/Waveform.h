3c4507f07ae983eaa96e2eb6cf7b9f1e1610c518
Dmitrij Ivlev <dmanivlev@edu.hse.ru>
2026-05-28 22:31:01 +0300

    Implemented the rule of 5 in Waveform.h

--- src/Waveform.h
+++ src/Waveform.h
@@ -16,9 +16,30 @@ class Waveform
     };
 public:
     Waveform() = default;
+    Waveform(const Waveform& other) : _dataInfo(other._dataInfo), _data(other._data) {}
+    Waveform& operator=(const Waveform& other)
+    {
+        if (&other != this)
+        {
+            _data = other._data;
+            _dataInfo = other._dataInfo;
+        }
+        return *this;
+    }
+    Waveform(Waveform&& other) noexcept : _dataInfo(std::move(other._dataInfo)), _data(std::move(other._data)) {}
+    Waveform& operator=(Waveform&& other) noexcept
+    {
+        if (&other != this)
+        {
+            _dataInfo = std::move(other._dataInfo);
+            _data = std::move(other._data);
+        }
+        return *this;
+    }
+    ~Waveform() = default;
 public:
     const std::vector<int16_t>& getData() const { return _data; }
-
+    const DataInfo& getDataInfo() const { return _dataInfo; }
 private:
     std::vector<int16_t> _data;
     DataInfo _dataInfo;
